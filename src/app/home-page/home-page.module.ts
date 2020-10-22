ca5cb875074b0e46e955c80481ea16acfdc26330
varshaRaj <minivarsha.a@netcon.in>
2020-10-22 20:40:46 +0530

    changes2

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageRoutingModule } from './home-page-routing.module';
+import { BodyContentComponent } from './body-content/body-content.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BodyContentComponent,NavMenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HomePageRoutingModule
   ]
 })
 export class HomePageModule { }
